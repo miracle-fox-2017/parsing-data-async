b3b26a0cd2baf5e6356c9712bf7db7cec383d88b
vbagustinus <vbagustinus@gmail.com>
2017-10-25 17:30:06 +0700

    parsing data Async

--- parser.js
+++ parser.js
@@ -1,15 +1,45 @@
 "use strict"
 
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor(obj){
+    this.id = obj.id
+    this.first_name = obj.first_name
+    this.last_name = obj.last_name
+    this.email = obj.email
+    this.phone = obj.phone
+    this.created_at = obj.created_at
+  }
 }
 
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = []
+  }
+
+  parsing(cb){
+    fs.readFile(this._file, 'utf8', (err, data)=>{
+      if(!err){
+        let pisah = data.split('\n')
+        for (var idx = 1; idx < pisah.length-1; idx++) {
+          let dataArray = pisah[idx].split(',');
+          let inputData = new Person(
+            {
+              id: dataArray[0],
+              first_name: dataArray[1],
+              last_name: dataArray[2],
+              email: dataArray[3],
+              phone: dataArray[4],
+              created_at: new Date(dataArray[5])
+            })
+          this._people.push(inputData)
+        }
+        cb()
+      } else {
+        console.log('Error get data CSV');
+      }
+    })
   }
 
   get people() {
@@ -21,5 +51,5 @@ class PersonParser {
 }
 
 let parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+console.log(parser.people);
+// console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
